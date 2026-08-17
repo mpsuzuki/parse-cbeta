b94606651528df32bdf10bf495a05af0976140c9
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2026-08-17 20:13:58 +0900

    tick_spinner.py: TickSpinner class

---
+++ tick_spinner.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass, field
+import sys
+from typing import TextIO
+
+
+@dataclass
+class TickSpinner:
+  file: TextIO = sys.stderr
+  tick_interval: int = 500
+  count: int = 0
+  spinner: tuple[str, ...] = ("/", "-", "\\", "|")
+  label: str = ""
+
+
+  def progress(self):
+    self.count += 1
+
+    if self.count % self.tick_interval != 0:
+      return
+
+    idx = (self.count // self.tick_interval) % len(self.spinner)
+
+    print(
+      f"\r{self.label} {self.spinner[idx]}",
+      end="",
+      flush=True,
+      file=self.file,
+    )
+
+  def finish(self):
+    print("\r ", file=self.file)
+
+  def set_spinner(self, *chars: str):
+    if chars:
+      self.spinner = chars
+
+  def set_label(self, label: str):
+    self.label = label
